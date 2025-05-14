f2c6b2da2bb5a534cc0f8cc24a6c7fdca32ccdda
constantly-dev <rntqhdl10@naver.com>
2025-05-14 14:05:30 +0900

    chore: prettier 적용 파일 정리 (#16)

--- src/shared/apis/http.ts
+++ src/shared/apis/http.ts
@@ -1,54 +1,36 @@
-import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
-import axios from "axios";
-import * as qs from "qs";
+import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios from 'axios';
+import * as qs from 'qs';
 
 // Axios 인스턴스 생성
 const api: AxiosInstance = axios.create({
-	baseURL: process.env.REACT_APP_BASE_URL, //추후 env에 백 배포 url 추가
-	timeout: 4000,
-	headers: {
-		"Content-Type": "application/json",
-	},
-	paramsSerializer: (params) => {
-		return qs.stringify(params, { indices: false });
-	},
+  baseURL: process.env.REACT_APP_BASE_URL, //추후 env에 백 배포 url 추가
+  timeout: 4000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  paramsSerializer: (params) => {
+    return qs.stringify(params, { indices: false });
+  },
 });
 
 // HTTP 헬퍼 함수 타입 정의
 interface HttpClientTypes {
-	//T = unknown: 요청 결과(response data)의 타입 제네릭
-	get<T = unknown>(
-		url: string,
-		config?: AxiosRequestConfig
-	): Promise<AxiosResponse<T>>;
-	post<T = unknown>(
-		url: string,
-		data?: unknown,
-		config?: AxiosRequestConfig
-	): Promise<AxiosResponse<T>>;
-	put<T = unknown>(
-		url: string,
-		data?: unknown,
-		config?: AxiosRequestConfig
-	): Promise<AxiosResponse<T>>;
-	patch<T = unknown>(
-		url: string,
-		data?: unknown,
-		config?: AxiosRequestConfig
-	): Promise<AxiosResponse<T>>;
-	delete<T = unknown>(
-		url: string,
-		config?: AxiosRequestConfig
-	): Promise<AxiosResponse<T>>;
+  //T = unknown: 요청 결과(response data)의 타입 제네릭
+  get<T = unknown>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>;
+  post<T = unknown>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>;
+  put<T = unknown>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>;
+  patch<T = unknown>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>;
+  delete<T = unknown>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>;
 }
 
 // 타입 적용된 http 객체
 const http: HttpClientTypes = {
-	get: (url, config) => api.get(url, config),
-	post: (url, data, config) => api.post(url, data, config),
-	put: (url, data, config) => api.put(url, data, config),
-	patch: (url, data, config) => api.patch(url, data, config),
-	delete: (url, config) => api.delete(url, config),
+  get: (url, config) => api.get(url, config),
+  post: (url, data, config) => api.post(url, data, config),
+  put: (url, data, config) => api.put(url, data, config),
+  patch: (url, data, config) => api.patch(url, data, config),
+  delete: (url, config) => api.delete(url, config),
 };
 
 export default http;
