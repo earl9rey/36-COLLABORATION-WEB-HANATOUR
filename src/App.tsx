ef42a05375da80df0a5ca8ccd8e3306428eb2544
yeeeww <yeangsshi@ewhain.net>
2025-05-14 10:18:19 +0900

    setting: tanstack query setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import queryClient from "./queryClient";
 import "./App.css";
 
 function App() {
-  return <></>;
+	return (
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
 }
 
 export default App;
