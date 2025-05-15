bf441a7c105aa4c5306a250536d8f01eddfb2a87
siyeon9302 <siyeon9302@gmail.com>
2025-05-15 22:24:14 +0900

    feat: 카드5 구현(#22)

---
+++ src/shared/components/Card/Card5.tsx
@@ -0,0 +1,14 @@
+const Card5 = () => {
+  return (
+    <div className="flex h-[26.5rem] w-[26.6rem] flex-col">
+      <img
+        className="object-cover"
+        src="https://cdn.sisaweek.com/news/photo/202304/203897_204891_41.jpg"
+        alt="할인내용"></img>
+      <p className="sub5-r-15 text-gray500 mt-[2.0rem]">서브텍스트</p>
+      <p className="head7-m-17 text-gray600 mt-[0.6rem]">타이틀</p>
+    </div>
+  );
+};
+
+export default Card5;
