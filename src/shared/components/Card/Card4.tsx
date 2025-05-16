735d6404aeb7ae2525df552b5084bc23a81a9d6f
siyeon9302 <siyeon9302@gmail.com>
2025-05-17 04:04:07 +0900

    feat: card4 props 추가 (#22)

--- src/shared/components/Card/Card4.tsx
+++ src/shared/components/Card/Card4.tsx
@@ -1,15 +1,19 @@
-const Card4 = () => {
+type TravelCard4Props = {
+  image: string;
+  title: string;
+  countryName: string;
+  price: string;
+};
+
+const Card4 = ({ image, title, countryName, price }: TravelCard4Props) => {
   return (
     <div className="flex h-[9.8rem] w-[30.4rem] flex-row">
-      <img
-        className="h-[9.8rem] w-[9.7rem] object-cover"
-        src="https://www.casenews.co.kr/news/photo/202408/16250_35442_5945.jpg"
-        alt="여행사진"></img>
+      <img className="h-[9.8rem] w-[9.7rem] object-cover" src={image} alt={title}></img>
       <div className="ml-[1.9rem]">
-        <p className="body3-sb-15 text-gray800 mt-[0.3rem]">#타이틀</p>
-        <p className="body5-r-14 text-gray500 mt-[0.5rem]">나라이름</p>
+        <p className="body3-sb-15 text-gray800 mt-[0.3rem]">#{title}</p>
+        <p className="body5-r-14 text-gray500 mt-[0.5rem]">{countryName}</p>
         <div className="mt-[1.1rem] mb-[2.3rem] flex flex-row items-center">
-          <p className="body3-sb-15 text-gray800">123,456</p>
+          <p className="body3-sb-15 text-gray800">{price}</p>
           <p className="body7-r-13 text-gray800">원~</p>
         </div>
       </div>
