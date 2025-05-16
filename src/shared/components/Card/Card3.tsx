81d5eff31bbe85001cd9754aa1e51930ba203afd
siyeon9302 <siyeon9302@gmail.com>
2025-05-17 04:01:13 +0900

    feat: card3 props 추가 (#22)

--- src/shared/components/Card/Card3.tsx
+++ src/shared/components/Card/Card3.tsx
@@ -1,14 +1,19 @@
-const Card3 = () => {
+type TravelCard3Props = {
+  image: string;
+  packageName: string;
+  title: string;
+  price: string;
+};
+
+const Card3 = ({ image, packageName, title, price }: TravelCard3Props) => {
   return (
     <div className="h-[32.9rem] w-[26.9rem]">
-      <img
-        src="https://flexible.img.hani.co.kr/flexible/normal/700/466/imgdb/original/2021/0603/20210603504045.jpg"
-        alt="예시 이미지"></img>
+      <img src={image} alt={title}></img>
       <div className="mt-[2.8rem]">
-        <p className="text-blue200 body7-r-13">패키지</p>
-        <p className="head7-m-17 my-[0.5rem]">타이틀</p>
+        <p className="text-blue200 body7-r-13">{packageName}</p>
+        <p className="head7-m-17 my-[0.5rem]">{title}</p>
         <div className="flex flex-row items-center">
-          <p className="text-gray800 head6-sb-22">123,456</p>
+          <p className="text-gray800 head6-sb-22">{price}</p>
           <p className="text-gray800 body2-r-17">원</p>
         </div>
       </div>
