91afaa64ffd2737306d4922519be7e37c00e8b9a
siyeon9302 <siyeon9302@gmail.com>
2025-05-17 03:41:59 +0900

    feat: card1 props 추가

--- src/shared/components/Card/Card1.tsx
+++ src/shared/components/Card/Card1.tsx
@@ -1,30 +1,37 @@
-const Card1 = () => {
+type TravelCard1Props = {
+  image: string;
+  hashTag: string;
+  title: string;
+  subText: string;
+  priceNum: string;
+  feature1: string;
+  feature2: string;
+};
+
+const Card1 = ({ image, hashTag, title, subText, priceNum, feature1, feature2 }: TravelCard1Props) => {
   return (
     <div className="h-[36.6rem] w-[26.9rem]">
-      <img
-        src="https://image1.lottetour.com/static/trvtour/201910/1715/011ee82200cbf4f301c382e19f44b28e"
-        alt="여행사진"
-        className="h-[20.1rem] w-[26.9rem]"></img>
+      <img src={image} alt={title} className="h-[20.1rem] w-[26.9rem]"></img>
       <div className="mt-[1.5rem]">
         {/* 해시태그 */}
-        <p className="text-blue200 body7-r-13">#태그</p>
+        <p className="text-blue200 body7-r-13">#{hashTag}</p>
         {/* 제목 */}
-        <h1 className="text-gray800 head7-m-17 my-[0.7rem]">제목</h1>
+        <h1 className="text-gray800 head7-m-17 my-[0.7rem]">{title}</h1>
         {/* 서브텍스트 */}
-        <p className="text-gray600 body5-r-14">#서브텍스트</p>
+        <p className="text-gray600 body5-r-14">#{subText}</p>
         <div className="mt-[1.5rem] flex flex-row items-center">
           {/* 가격 숫자*/}
-          <p className="text-gray800 head6-sb-22">121322</p>
+          <p className="text-gray800 head6-sb-22">{priceNum}</p>
           {/* 가격 원 */}
           <p className="text-gray800 body2-r-17">원</p>
         </div>
         {/* 특징 */}
         <div className="mt-[1.2rem] flex flex-row">
           <div className="border-gray500 border-0.1 mr-[0.9rem] border p-[0.7rem]">
-            <p className="text-gray500">텍스트</p>
+            <p className="text-gray500">{feature1}</p>
           </div>
           <div className="border-gray500 border-0.1 border p-[0.7rem]">
-            <p className="text-gray500">텍스트</p>
+            <p className="text-gray500">{feature2}</p>
           </div>
         </div>
       </div>
