c16559c9a16aaab3c1414ee8a085a8938ad80735
siyeon9302 <siyeon9302@gmail.com>
2025-05-17 04:06:47 +0900

    feat: card5 props 추가 (#22)

--- src/shared/components/Card/Card5.tsx
+++ src/shared/components/Card/Card5.tsx
@@ -1,12 +1,15 @@
-const Card5 = () => {
+type TravelCard5Props = {
+  image: string;
+  subtext: string;
+  title: string;
+};
+
+const Card5 = ({ image, subtext, title }: TravelCard5Props) => {
   return (
     <div className="flex h-[26.5rem] w-[26.6rem] flex-col">
-      <img
-        className="object-cover"
-        src="https://cdn.sisaweek.com/news/photo/202304/203897_204891_41.jpg"
-        alt="할인내용"></img>
-      <p className="sub5-r-15 text-gray500 mt-[2.0rem]">서브텍스트</p>
-      <p className="head7-m-17 text-gray600 mt-[0.6rem]">타이틀</p>
+      <img className="object-cover" src={image} alt={title}></img>
+      <p className="sub5-r-15 text-gray500 mt-[2.0rem]">{subtext}</p>
+      <p className="head7-m-17 text-gray600 mt-[0.6rem]">{title}</p>
     </div>
   );
 };
