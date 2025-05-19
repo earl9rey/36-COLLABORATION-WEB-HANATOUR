9450b85cf782726471ce37945b292366f26bbe8f
earl9rey <earl9rey@naver.com>
2025-05-19 16:08:30 +0900

    feat: 캘린더 월간 이동 로직 구현 (#32)

--- src/pages/home/components/CalendarModal.tsx
+++ src/pages/home/components/CalendarModal.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import '@/shared/styles/react-calendar.css';
@@ -6,26 +7,54 @@ const CalendarModal = () => {
   const tileClassName = ({ date, view }: { date: Date; view: string }) => {
     if (view === 'month') {
       const day = date.getDay();
-      if (day === 0 || day === 6) return 'red-day'; // 일(0), 토(6)는 빨간색
-      if (day === 5) return 'black-day'; // 금(5)은 검정색
+      if (day === 0 || day === 6) return 'red-day';
+      if (day === 5) return 'black-day';
     }
     return null;
   };
+  const today = new Date();
+  const [activeStartDate, setActiveStartDate] = useState(new Date());
+  const handlePrev = () => {
+    const prevDate = new Date(activeStartDate.getFullYear(), activeStartDate.getMonth() - 1, 1);
+    setActiveStartDate(prevDate);
+  };
+
+  const handleNext = () => {
+    const nextDate = new Date(activeStartDate.getFullYear(), activeStartDate.getMonth() + 1, 1);
+    setActiveStartDate(nextDate);
+  };
+
+  const isPrevDisabled =
+    activeStartDate.getFullYear() === today.getFullYear() && activeStartDate.getMonth() === today.getMonth();
+
+  const secondStartDate = new Date(activeStartDate.getFullYear(), activeStartDate.getMonth() + 1, 1);
+
   return (
-    <div className="border-gray800 flex h-[49.1rem] w-[70.8rem] flex-col border shadow-[0_0.4rem_2rem_rgba(0,0,0,0.25)]">
+    <div className="border-gray800 flex w-[70.8rem] flex-col border shadow-[0_0.4rem_2rem_rgba(0,0,0,0.25)]">
       <div className="flex gap-[3.6rem] px-[2.2rem] pt-[3.2rem] pb-[3rem]">
         <div className="flex-1">
+          {' '}
+          <button onClick={handlePrev} className="px-2" disabled={isPrevDisabled}>
+            {'<'}
+          </button>
           <Calendar
             calendarType="hebrew"
             tileClassName={tileClassName}
             formatDay={(_locale, date) => date.getDate().toString()}
+            activeStartDate={activeStartDate}
+            onActiveStartDateChange={() => {}}
           />
         </div>
         <div className="flex-1">
+          <button onClick={handleNext} className="px-2">
+            {'>'}
+          </button>
           <Calendar
             calendarType="hebrew"
             tileClassName={tileClassName}
             formatDay={(_locale, date) => date.getDate().toString()}
+            activeStartDate={secondStartDate}
+            onActiveStartDateChange={() => {}}
           />
         </div>
       </div>
@@ -34,7 +63,7 @@ const CalendarModal = () => {
           <p className="text-gray500 sub5-r-15 mb-[0.9rem]">여행 시작일</p>
           <p className="text-gray600 sub3-sb-15">2002.06.24(월)</p>
         </div>
-        <button className="bg-purple100 body2-r-17 h-[4.2rem] w-[11rem] rounded-[0.5rem] py-[1rem] text-white">
+        <button className="bg-purple100 body2-r-17 h-[4.2rem] w-[11rem] cursor-pointer rounded-[0.5rem] py-[1rem] text-white">
           선택완료
         </button>
       </div>
