d759e9426cec647616feeafbe187ffebe418f66a
si yeon <90365472+siyeon9302@users.noreply.github.com>
2025-05-21 01:52:40 +0900

    feat: 이미지 import

---
+++ src/shared/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import brandImg01 from '@/shared/assets/images/footerBrandImage01.png';
+import brandImg02 from '@/shared/assets/images/footerBrandImage02.png';
+import brandImg03 from '@/shared/assets/images/footerBrandImage03.png';
+import brandImg04 from '@/shared/assets/images/footerBrandImage04.png';
+import brandImg05 from '@/shared/assets/images/footerBrandImage05.png';
+import brandImg06 from '@/shared/assets/images/footerBrandImage06.png';
+import brandImg07 from '@/shared/assets/images/footerBrandImage07.png';
+import brandImg08 from '@/shared/assets/images/footerBrandImage08.png';
+import brandImg09 from '@/shared/assets/images/footerBrandImage09.png';
+
+const Footer = () => {};
+
+export default Footer;
