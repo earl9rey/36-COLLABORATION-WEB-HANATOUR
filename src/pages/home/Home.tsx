6f71acb0171c73d9cf36685f0b2b9528b63238a0
constantly-dev <rntqhdl10@naver.com>
2025-05-22 21:25:44 +0900

    feat: 광고 배너 하드 코딩 구현 (#58)

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,13 @@
+import PromotionBanner from '@/pages/home/components/PromotionBanner';
+import AdvertisementBanner from './components/AdvertisementBanner';
+
 const Home = () => {
-  return <div className="head1-sb-36">Home</div>;
+  return (
+    <div className="head1-sb-36 w-full">
+      <PromotionBanner />
+      <AdvertisementBanner />
+    </div>
+  );
 };
 
 export default Home;
