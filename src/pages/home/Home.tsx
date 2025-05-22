63d0067fd866c328148750fb6b746afe55de43ea
constantly-dev <rntqhdl10@naver.com>
2025-05-22 21:26:53 +0900

    chore: home 복구 (#58)

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,5 @@
-import PromotionBanner from '@/pages/home/components/PromotionBanner';
-import AdvertisementBanner from './components/AdvertisementBanner';
-
 const Home = () => {
-  return (
-    <div className="head1-sb-36 w-full">
-      <PromotionBanner />
-      <AdvertisementBanner />
-    </div>
-  );
+  return <div className="head1-sb-36 w-full"></div>;
 };
 
 export default Home;
